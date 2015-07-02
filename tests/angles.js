d3e76ad9b9c21c81159ffc6367599920fdb42768
mbr4nt <mateus.brant@gmail.com>
2015-07-02 16:40:55 +0000

    version working with options object

---
+++ tests/angles.js
@@ -0,0 +1,3 @@
+var views = require("../views.js");
+var bestView = views.bestView(5);
+console.log(bestView, views.allViews());
--- tests/farm.js
+++ tests/farm.js
@@ -1,10 +1,16 @@
 var farmConnector = require("../farmConnector.js");
 var baseS7AccountUrl = "http://s7d4.scene7.com/ir/render/HONRender";
 var vignette = "HIWM3";
-var angle = "0030";
-var properties = {};
+var angle = 45;
+var options = {
+    "UP00": "#92a342",
+    "ME00": "#151515",
+    "back": "M",
+    "base": "SB",
+    "amrtype": "F"
+};
 var size = 100;
 var imageType = "png-alpha";
-farmConnector.url(baseS7AccountUrl, vignette, angle, properties, size, imageType, function(err, url) {
+farmConnector.url(baseS7AccountUrl, vignette, angle, options, size, imageType, function(err, url) {
     console.log(url);    
 });
