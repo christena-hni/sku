e5af3eac004232262f4be1ff757e6bb8c0cee8b3
tucaz <me@tucaz.net>
2015-08-14 13:45:25 -0500

    Added correct light handling and changed the name of the material to a friendly one

--- data/indexData.js
+++ data/indexData.js
@@ -3,7 +3,7 @@ var fs = require("fs");
 var _ = require("underscore");
 var converter = new Converter();
 
-var fileName = "./materials.csv";
+var fileName = "./textiles.csv";
 
 var isFabric = function() {
   return this.Scene7Name != null && this.Scene7Name != undefined && this.Scene7Name != "";
@@ -31,8 +31,22 @@ var material = function() {
 };
 
 var name = function() {
-  return (this.Supplier + "_" + this.FabricPattern + "_" + this.FabricColor).toLowerCase();
+  return this.Supplier + " " + this.FabricPattern + " " + this.FabricColor;
 };
+
+var light = function() {
+  if(this.Type === "Fabric") {
+    return "01";
+  } else if(this.Type === "Leather") {
+    return "11";
+  } else if(this.Type === "Coded") {
+    return "21";
+  } else {
+    //TODO: Once we get all data right we need to throw an exception here
+    //throw new Error(this.Type + " is not a valid type of material");
+    return "01";
+  }
+}
 
 converter.on("end_parsed", function(json) {
   for(var i = 0;i < json.length;i++) {
@@ -42,29 +56,28 @@ converter.on("end_parsed", function(json) {
     json[i].hasMaterial = hasMaterial;
     json[i].material = material;
     json[i].name = name;
+    json[i].light = light;
   };
 
-  var materials = {};
+  var textiles = {};
   var itemsWithMaterial = _.filter(json, function(item) { return item.hasMaterial(); });
 
   _.each(itemsWithMaterial, function(item) {
     if(item.AllsteelCode != null && item.AllsteelCode != undefined && item.AllsteelCode != "") {
-      materials[item.AllsteelCode] = { light: "01", material: item.material(), name: item.name() };
+      textiles[item.AllsteelCode] = { light: item.light(), material: item.material(), name: item.name() };
     }
 
     if(item.HONCode != null && item.HONCode != undefined && item.HONCode != "") {
-      materials[item.HONCode] = { light: "01", material: item.material(), name: item.name() };
+      textiles[item.HONCode] = { light: item.light(), material: item.material(), name: item.name() };
     }
   });
 
-  console.log(JSON.stringify(materials));
-
-  fs.writeFile("materials.json", JSON.stringify(materials), function(err) {
+  fs.writeFile("textiles.json", JSON.stringify(textiles), function(err) {
     if(err) {
         return console.log(err);
     };
 
-    console.log("materials.json saved")
+    console.log("textiles.json saved")
   });
 });
 
