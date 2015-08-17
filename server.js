84e4bc4b10768eb4fae8ba6b4e536fadd6142aea
tucaz <me@tucaz.net>
2015-08-17 13:28:05 -0500

    First version of the JSON API with methods to get images for products

--- server.js
+++ server.js
@@ -10,74 +10,104 @@ var skuImages = require("./skuImages.js");
 var baseSkuMapsDirectory = "./data/skuMaps/";
 
 //TODO: Scan and load all SKU Maps from the base folder instead of specify each one manually
-var skus = _.map(["seek.skumap", "19.skumap", "nimble-polished.skumap", "nimble-powdercoat.skumap"], function(sku) {
-  return path.join(baseSkuMapsDirectory, sku);
+var skus = _.map(["seek.skumap", "19.skumap", "nimble-polished.skumap", "nimble-powdercoat.skumap"], function (sku) {
+    return path.join(baseSkuMapsDirectory, sku);
 });
 
-skuMapProcessor.processAll(skus,function(err, map) {
-  if(err) {
-    console.log(err);
-  }
-  else {
-    var router = express();
-    var server = http.createServer(router);
+skuMapProcessor.processAll(skus, function (err, map) {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        var app = express();
+        var server = http.createServer(app);
 
-    router.use(express.static(path.resolve(__dirname, 'client')));
+        app.use(express.static(path.resolve(__dirname, 'client')));
+        app.use('/bower_components', express.static(__dirname + '/bower_components'));
+        app.use('/polymers', express.static(__dirname + '/polymers'));
 
-    router.set('view engine', 'jade');
+        app.set('view engine', 'jade');
 
-    server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
-      var addr = server.address();
-      console.log("Product image API server listening at", addr.address + ":" + addr.port);
-    });
-
-    router.get("/breakdown/:sku", function(req, res) {
-      var properties = skuParser.testAndParse(req.params.sku, map);
-      console.log(properties);
-      res.json(properties);
-    });
-
-    router.get("/url/:sku/:size/all", function(req, res) {
-      var sku = req.params.sku;
-      var properties = skuParser.testAndParse(sku, map);
-      var size = req.params.size;
-      skuImages.all(sku, map, size, function(err, urls){
-        res.json({
-          urls: urls
+        server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function () {
+            var addr = server.address();
+            console.log("Product image API server listening at", addr.address + ":" + addr.port);
         });
-      });
-    });
 
-    router.get("/page/:sku/:size/all", function(req, res) {
-      var sku = req.params.sku;
-      var properties = skuParser.testAndParse(sku, map);
-      var size = req.params.size;
-      skuImages.all(sku, map, size, function(err, urls){
-        res.render("allviews", {
-          urls: urls
+        app.get("/breakdown/:sku", function (req, res) {
+            var properties = skuParser.testAndParse(req.params.sku, map);
+            console.log(properties);
+            res.json(properties);
         });
-      });
-    });
 
-    router.get("/url/:sku/:size/:angle", function(req, res) {
-      var sku = req.params.sku;
-      var properties = skuParser.testAndParse(sku, map);
-      var size = req.params.size;
-      var angle = req.params.angle;
-      skuImages.one(sku, map, size, angle, function(err, url){
-        res.json({
-          url: url
+        app.get("/url/:sku/:size/all", function (req, res) {
+            var sku = req.params.sku;
+            var properties = skuParser.testAndParse(sku, map);
+            var size = req.params.size;
+            skuImages.all(sku, map, size, function (err, urls) {
+                res.json({
+                    urls: urls
+                });
+            });
         });
-      });
-    });
 
-    router.get('/', function (req, res) {
-      res.render('index', { title: 'All Products', products: [{ name: "#19" }, { name: "Acuity" }] });
-    });
+        app.get("/page/:sku/:size/all", function (req, res) {
+            var sku = req.params.sku;
+            var properties = skuParser.testAndParse(sku, map);
+            var size = req.params.size;
+            skuImages.all(sku, map, size, function (err, urls) {
+                res.render("allviews", {
+                    urls: urls
+                });
+            });
+        });
 
-    // router.get('/configurator/:product', function (req, res) {
-    //   var file = req.params.product + '.html';
-    //   res.sendFile(file, { root: path.join(__dirname, 'views', 'configurator') });
-    // });
-  }
+        app.get("/url/:sku/:size/:angle", function (req, res) {
+            var sku = req.params.sku;
+            var properties = skuParser.testAndParse(sku, map);
+            var size = req.params.size;
+            var angle = req.params.angle;
+            skuImages.one(sku, map, size, angle, function (err, view) {
+                res.json({
+                    url: view.url
+                });
+            });
+        });
+
+        app.get('/', function (req, res) {
+            res.render('index', { title: 'All Products', products: [{ name: "#19" }, { name: "Acuity" }] });
+        });
+
+        // Public API Methods
+
+        // Examples:
+        // /api/products/A19-HWG.2.P19.DOL719 - Returns image with default angle and size
+        // /api/products/A19-HWG.2.P19.DOL719?size=1200&angle=0030 - Returns image with specified angle and custom size
+        app.get("/api/products/:sku", function (req, res) {
+            var sku = req.params.sku;
+            var properties = skuParser.testAndParse(sku, map);
+            //optional querystring parameter
+            var size = req.query.size;
+            //optional querystring parameter
+            var angle = req.query.angle;
+
+            skuImages.one(sku, map, size, angle, function (err, url) {
+                res.setHeader('Content-Type', 'application/json');
+                res.send(JSON.stringify({ url: url }));
+            });
+        });
+
+        // Examples:
+        // /api/products/A19-HWG.2.P19.DOL719/all - Returns an array with images of all angles and default size
+        // /api/products/A19-HWG.2.P19.DOL719?size=1200- Returns an array with images of all angles and custom size
+        app.get("/api/products/:sku/all", function (req, res) {
+            var sku = req.params.sku;
+            var properties = skuParser.testAndParse(sku, map);
+            var size = req.query.size;
+
+            skuImages.all(sku, map, size, function (err, urls) {
+                res.setHeader('Content-Type', 'application/json');
+                res.send(JSON.stringify(urls));
+            });
+        });
+    }
 });
--- skuImages.js
+++ skuImages.js
@@ -1,30 +1,36 @@
 module.exports = new (function(skuParser, async, views, farmConnector){
+    var baseS7AccountUrl = "http://s7d4.scene7.com/ir/render/AllsteelRender";
+    var defaultSize = 1000;
+    var defaultImageFormat = "png-alpha";
+
     this.one = one;
     this.all = all;
-    var baseS7AccountUrl = "http://s7d4.scene7.com/ir/render/AllsteelRender";
+
     function one(sku, map, size, angle, callback) {
         var options = skuParser.testAndParse(sku, map);
-        var view = views.bestView(angle);
+        var view = angle != null ? views.bestView(angle) : views.defaultView();
         var vignette = options.constants.vignette;
-        get(baseS7AccountUrl, vignette, view, options.staticObjects, options.materials, size, "png-alpha", callback);
-    }
-    function defaultView(sku, size, callback) {
-        var view = views.defaultView();
-        get(baseS7AccountUrl, options, view, callback);
-    }
+        size = size != null ? size : defaultSize;
+
+        get(baseS7AccountUrl, vignette, view, options.staticObjects, options.materials, size, defaultImageFormat, callback);
+    };
+
     function all(sku, map, size, callback) {
         var options = skuParser.testAndParse(sku, map);
         var vignette = options.constants.vignette;
         var allViews = views.allViews();
+        var size = size != null ? size : defaultSize;
+
         async.map(
             allViews,
             function(view, itemCallback) {
-                get(baseS7AccountUrl, vignette, view, options.staticObjects, options.materials, size, "png-alpha", itemCallback);
+                get(baseS7AccountUrl, vignette, view, options.staticObjects, options.materials, size, defaultImageFormat, itemCallback);
             },
             callback
-            );
-    }
+        );
+    };
+
     function get(baseS7AccountUrl, vignette, view, staticObjects, materials, size, imageType, callback) {
         farmConnector.url(baseS7AccountUrl, vignette, view, staticObjects, materials, size, imageType, callback);
-    }
+    };
 })(require("./skuParser.js"), require("async"), require("./views.js"), require("./farmConnector.js"));
